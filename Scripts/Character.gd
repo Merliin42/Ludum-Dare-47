07b4b9d89753ca370042e94bbdbfa5be59a80a29
Lucas DUPUIS <lucasdupuis7@gmail.com>
2020-10-03 11:33:24 +0200

    Creation and detection of tasks

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -15,3 +15,12 @@ func _process(delta): # Cette fonction est exécutée à chaque image calculée 
 		displacment.x = 1
 	displacment = displacment.normalized()*VELOCITY # Normalise le vecteur pour garder toujours la même vitesse et multiplie par la vélocité
 	move_and_slide(displacment) # Déplace le personnage
+
+
+func _on_DetectTasks_area_entered(area): # Cette fonction s'exécute quand le joueur détecte une tache
+#	print("Thing detected")
+	pass
+
+func _on_DetectTasks_area_exited(area): # Cette fonction s'exécute quand le joueur sors de la zone de détection de la tache
+#	print("Thing gone")
+	pass
