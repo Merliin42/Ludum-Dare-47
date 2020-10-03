dd57f2b6b5dc7d39608e78f4893a905e77a2e3e7
Lucas DUPUIS <lucasdupuis7@gmail.com>
2020-10-03 11:33:24 +0200

    Creation and detection of tasks

---
+++ Scripts/Interaction.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+export var texture : Texture
+
+func _ready():
+	$Sprite2.texture = texture
+	$Sprite.texture = texture
+
+
+func _on_Node2D_area_entered(area):
+	$Sprite2.visible = true
+
+
+func _on_Node2D_area_exited(area):
+	$Sprite2.visible = false
