f3dc96f7129c1cbfb702f63762fe1479c7d6ca2c
Lucas DUPUIS <lucasdupuis7@gmail.com>
2020-10-03 10:57:33 +0200

    Comment code

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,9 +1,10 @@
 extends KinematicBody2D
 
-const VELOCITY : int = 200
+const VELOCITY : int = 200 # A changer pour modifier la vitesse du personnage
 
-func _process(delta):
-	var displacment : Vector2 = Vector2(0, 0)
+func _process(delta): # Cette fonction est exécutée à chaque image calculée par le moteur
+	var displacment : Vector2 = Vector2(0, 0) # Un vecteur qui illustre les input du joueur
+	# Ce bloc permet de prendre les input du joueur
 	if Input.is_action_pressed("ui_up"):
 		displacment.y = -1
 	if Input.is_action_pressed("ui_down"):
@@ -12,5 +13,5 @@ func _process(delta):
 		displacment.x = -1
 	if Input.is_action_pressed("ui_right"):
 		displacment.x = 1
-	displacment = displacment.normalized()*VELOCITY
-	move_and_slide(displacment)
+	displacment = displacment.normalized()*VELOCITY # Normalise le vecteur pour garder toujours la même vitesse et multiplie par la vélocité
+	move_and_slide(displacment) # Déplace le personnage
