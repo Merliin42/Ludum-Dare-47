b4e11743b7332b27d4fcf9c18b0f5a735d85bea7
Lucas DUPUIS <lucasdupuis7@gmail.com>
2020-10-04 12:36:20 +0200

    Change walls

--- Scripts/Room.gd
+++ Scripts/Room.gd
@@ -2,9 +2,17 @@ extends StaticBody2D
 
 
 
-func _on_PlayerDetect_area_entered(area):
-	self.modulate = Color(1, 1, 1, 1)
+func _on_WallLeftDetector_area_entered(area):
+	$WallLeft.modulate = Color(1, 1, 1, .3)
 
 
-func _on_PlayerDetect_area_exited(area):
-	self.modulate = Color(1, 1, 1, 0.3)
+func _on_WallLeftDetector_area_exited(area):
+	$WallLeft.modulate = Color(1, 1, 1, 1)
+
+
+func _on_WallRightDetector_area_entered(area):
+	$WallRight.modulate = Color(1, 1, 1, .3)
+
+
+func _on_WallRightDetector_area_exited(area):
+	$WallRight.modulate = Color(1, 1, 1, 1)
