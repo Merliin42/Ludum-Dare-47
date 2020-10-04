df38cf6014abfca4d5a0fd10e4540f63cff28274
Lucas DUPUIS <lucasdupuis7@gmail.com>
2020-10-04 11:02:18 +0200

    prout

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -7,9 +7,12 @@ var today_tasks = []
 func _ready():
 	randomize()
 	for i in range(4) :
-		var random = randi() % tasks.size() - i
+		var random = randi() % (tasks.size() - i)
 		today_tasks.append(tasks[random])
 		tasks.remove(random)
-	
-	
 	$HUD.set_tasks(today_tasks)
+
+func _on_Character_task(task_name):
+	if today_tasks.find(task_name) != -1 :
+		$HUD.task_done(task_name)
+		today_tasks.remove(today_tasks.find(task_name))
